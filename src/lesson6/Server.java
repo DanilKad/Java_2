1867af0e6691e590409f7c5d024bc9d7b0599b8b
Danil Kadukov <25lotov@gmail.com>
2019-12-07 18:39:51 +0300

    сделал и создал ветку

---
+++ src/lesson6/Server.java
@@ -0,0 +1,35 @@
+package lesson6;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+
+        ServerSocket server = null;
+        Socket socket = null;
+
+
+        try {
+            server = new ServerSocket(9189);
+            System.out.println("Сервер запущен");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент онлайн");
+                new Thread(new ClientHandler(socket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+                server.close();
+                System.out.println("Сервер закрыт");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/lesson6/Start.java
@@ -0,0 +1,8 @@
+package lesson6;
+
+public class Start {
+    public static void main(String[] args) {
+
+        new Client();
+    }
+}
